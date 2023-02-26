54ec814d7023037d207cd43785ae9c072f11a81e
Dhruvin111 <108988297+Dhruvin111@users.noreply.github.com>
2023-02-26 23:17:57 +0530

    Add files via upload

---
+++ Day2/name pages/Home.js
@@ -0,0 +1,12 @@
+import react, {Component} from 'react'
+import msg from '../component/Title'
+
+class msg1 extends Component{
+    render(){
+        return(
+            <msg />
+        )
+    }
+     
+}
+export default  
